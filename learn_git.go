2bb06721acb2184dc27cb38b542d7e791807e6a9
GeekUniversity <zkm1434479187@qq.com>
2023-10-08 21:26:41 -0700

    creat learn_git.go

---
+++ learn_git.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	//	绑定请求和处理函数
+	http.HandleFunc("/user/login",
+		func(writer http.ResponseWriter,
+			request *http.Request){
+			/*数据库操作
+			逻辑处理
+			restfulApi json/xml返回
+			 */
+			io.WriteString(writer,"hello,world!")
+		})
+
+	http.ListenAndServe(":8080",nil)
+
+}
